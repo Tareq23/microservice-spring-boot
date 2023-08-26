0f2047976b268e096b8c3ad64a9fef3316c84965
Md Tarequl Islam <mdtarequlislam634@gmail.com>
2023-08-27 00:42:10 +0600

    department-service init

--- department-service/src/main/java/com/departmentservice/DepartmentServiceApplication.java
+++ department-service/src/main/java/com/departmentservice/DepartmentServiceApplication.java
@@ -1,16 +1,15 @@
-package com.tareq23.serviceregistry;
+package com.departmentservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class ServiceRegistryApplication {
+@EnableEurekaClient
+public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceRegistryApplication.class, args);
+		SpringApplication.run(DepartmentServiceApplication.class, args);
 	}
 
 }
